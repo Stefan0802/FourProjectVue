a7e8e4b10df494d68e080b0caca0d1ed39bae4a5
Stefan0802 <stpa.leonov@mail.ru>
2025-03-20 23:54:57 +0700

    created proj and start create login(autorisation)

---
+++ src/components/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <form class="login" @submit.prevent="login">
+      <h1>Sign in</h1>
+      <label>User name
+        <input type="text" required v-model="username">
+      </label>
+    <label>Password
+      <input type="text">
+    </label>
+  </form>
+</template>
